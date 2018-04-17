571465cd7b9c753689c9858dc23b11ec3ad3ba33
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2018-04-17 17:14:19 +0200

    Added an FPS counter

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FPSCounter : MonoBehaviour {
+
+    private Text counterText;
+    private double frameDurationAverage = -1;
+    private double frameDurationEstimatedAverage = -1;
+    private double t = 0.825;
+    private ulong frameCount = 0;
+
+    void Start()
+    {
+        counterText = GetComponent<Text>();
+    }
+
+    void Update ()
+    {
+        double frameDuration = Time.deltaTime;
+        ++frameCount;
+        
+        if(frameDurationAverage < 0)
+        {
+            frameDurationAverage = frameDuration;
+            frameDurationEstimatedAverage = frameDuration;
+        }	
+        else
+        {
+            frameDurationEstimatedAverage = t * frameDurationEstimatedAverage + (1 - t) * frameDuration;
+            frameDurationAverage = (((double)(frameCount - 1)) / frameCount) * (frameDurationAverage + frameDuration / (frameCount - 1));
+        }
+
+        counterText.text = "FPS Counter\n" +
+            "Average: " + (1.0 / frameDurationAverage).ToString("F2") + "\n" +
+            "Estimated Average: " + (1.0 / frameDurationEstimatedAverage).ToString("F2") + "\n";
+
+    }
+}
