b6faa842de988c61c9863c4adfd634dbee8504f8
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2018-04-17 17:32:35 +0200

    Added a script for the day/night cycle

---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+    public float dayLengthInSeconds;
+
+    private float rotationalSpeed;
+
+	void Start ()
+    {
+        rotationalSpeed = 360 / dayLengthInSeconds;
+	}
+	
+	void Update ()
+    {
+        transform.Rotate(rotationalSpeed * Time.deltaTime, 0, 0);
+	}
+}
